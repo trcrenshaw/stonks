2bc9c58d4ba2a8bad1fe9b6c005b4905efdb94ea
Tim Crenshaw <trcrenshaw@crimson.ua.edu>
2021-03-29 13:32:24 -0500

    Added S3; Stock Alert Updates

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,4 @@
-import discord
+
 from typing import List, Any
 from discord.ext import commands, tasks
 import discord
@@ -7,7 +7,12 @@ from dataclasses import dataclass
 from datetime import datetime
 import pickle
 import os
+import boto3
 
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 @dataclass
 class Alert:
@@ -18,13 +23,25 @@ class Alert:
 
 
 TOKEN = os.environ['TOKEN']
+S3_BUCKET = os.environ['S3_BUCKET']
+ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+
 client = commands.Bot(command_prefix='#')
 alerts: List[Alert] = []
+
+try:
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=ACCESS_KEY)
+    s3.download_file(S3_BUCKET, 'Alerts.pkl', 'Alerts.pkl')
+except:
+    alerts: List[Alert] = []
+
 try:
     with open('Alerts.pkl', 'rb') as f:
         alerts = pickle.load(f)
 except:
     alerts: List[Alert] = []
+
 
 @client.event
 async def on_ready():
@@ -34,9 +51,10 @@ async def on_ready():
 def save_alerts():
     with open('Alerts.pkl', 'wb') as f:
         pickle.dump(alerts, f, pickle.HIGHEST_PROTOCOL)
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=ACCESS_KEY)
+    s3.upload_file('Alerts.pkl', S3_BUCKET, 'Alerts.pkl')
 
-
-@commands.command()
+@commands.command(aliases=['AddAlert', 'addAlert'])
 async def add_alert(ctx, *args):
     if len(args) != 2:
         await ctx.send(f'Check your syntax on that command')
@@ -50,7 +68,7 @@ async def add_alert(ctx, *args):
     save_alerts()
 
 
-@commands.command()
+@commands.command(aliases=['RemoveAlert', 'removeAlert'])
 async def remove_alert(ctx, *args):
     global alerts
     for arg in args:
@@ -63,7 +81,7 @@ async def remove_alert(ctx, *args):
     save_alerts()
 
 
-@commands.command()
+@commands.command(aliases=['GetAlerts', 'getAlerts'])
 async def get_alerts(ctx, *args):
     msg = ''
     for index, alert in enumerate(alerts):
@@ -72,7 +90,7 @@ async def get_alerts(ctx, *args):
     await ctx.send(msg)
 
 
-@commands.command()
+@commands.command(aliases=['ResetAlerts', 'resetAlerts'])
 async def reset_alerts(ctx, *args):
     for alert in alerts:
         alert.last_alert = None
@@ -98,27 +116,42 @@ async def check_stocks():
         if alert.type == '%':
             change = round(100*(close[-1] - prev_value)/prev_value, 2)
             if abs(change) > alert.value:
-                embed = discord.Embed()
-                embed.description = f'{alert.ticker} changed by {change}%  Price: ${round(close[-1], 2)}'
-                await channel.send(embed=embed)
+                em = discord.Embed()
+                em.colour = 0x00ff00 if change > alert.value else 0xff0000
+                em.description = f'[{alert.ticker}](https://finance.yahoo.com/quote/{alert.ticker}) changed by ' \
+                                 f'{change}%  Price: ${round(close[-1], 2)}'
+                await channel.send(embed=em)
                 alert.last_alert = close[-1]
         elif alert.type == '$':
             change = round(close[-1] - prev_value, 2)
             if abs(change) > alert.value:
-                embed = discord.Embed()
-                embed.description = f'{alert.ticker} changed by ${change}  Price: ${round(close[-1], 2)}'
-                await channel.send(embed=embed)
+                em = discord.Embed()
+                em.colour = 0x00ff00 if change > alert.value else 0xff0000
+                em.description = f'[{alert.ticker}](https://finance.yahoo.com/quote/{alert.ticker}) changed by ' \
+                                 f'${change} Price: ${round(close[-1], 2)}'
+                await channel.send(embed=em)
                 alert.last_alert = close[-1]
     save_alerts()
 
 
-@commands.command()
+@commands.command(aliases=['CheckAlerts', 'checkAlerts'])
+async def check_alerts(ctx, *args):
+    await check_stocks()
+
+
+@commands.command(aliases=['GetStock', 'getStock', 'GetPrice', 'getPrice'])
 async def get_stock(ctx, *args):
     ticker = args[0]
-    em = discord.Embed(title="{ticker}",
-                       colour=0x0080c0)
-    em.add_field(name="Ticker", value="https://finance.yahoo.com/quote/{ticker}")
-    await ctx.send(embed=em)
+
+    try:
+        data = yf.download(ticker, period='1d', interval='1m')
+        close = data['Close']
+
+        em = discord.Embed(colour=0x0080c0,
+                           description=f'[{ticker}](https://finance.yahoo.com/quote/{ticker}) is ${round(close[-1], 2)}')
+        await ctx.send(embed=em)
+    except:
+        await ctx.send(f'Could not get data for {ticker}')
 
 
 @check_stocks.before_loop
